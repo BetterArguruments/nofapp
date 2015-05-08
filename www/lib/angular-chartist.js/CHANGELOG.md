2fab819fd309ebab4367829c5d70caad17e58719
Oliver Eidel <oli@eidel.net>
2015-05-08 22:32:29 +0200

    Added Chartist (new charting library, yet again) and dependencies (angular)

---
+++ www/lib/angular-chartist.js/CHANGELOG.md
@@ -0,0 +1,62 @@
+### 3.1.0 (2015-04-06)
+
+
+#### Bug Fixes
+
+* **angular-chartist:**
+  * Fixed issue with update and recreation performed in sequence ([b30f505a](https://github.com/paradox41/angular-chartist.js/commit/b30f505a6dda3e568b1eff46b1a83e6350756e6e))
+  * Fix typo ([7f17e7ec](https://github.com/paradox41/angular-chartist.js/commit/7f17e7ec31813eb71d5e52d2872a0b2b8f97e7e1))
+  * Call detach on ([9529e630](https://github.com/paradox41/angular-chartist.js/commit/9529e6308be42487378880329eef669fc61894cd), closes [#40](https://github.com/paradox41/angular-chartist.js/issues/40))
+
+
+### 3.0.3 (2015-02-17)
+
+#### Bug Fixes
+
+* **bower:** Fix location of main ([ec5210b3](https://github.com/paradox41/angular-chartist.js/commit/ec5210b379efecc0ed276727068599c14f866358))
+
+
+#### Features
+
+* **angular-chartist:**
+  * Rerender chart on options change ([af7b6fdf](https://github.com/paradox41/angular-chartist.js/commit/af7b6fdf9f6cf2eaf00f46ac7bc999f2a5fcdf72))
+  * Rerender chart on options change ([1815bf76](https://github.com/paradox41/angular-chartist.js/commit/1815bf76b222499c9f8da14e70138e8970f6628a))
+
+
+### 3.0.1 (2015-02-08)
+
+
+## 3.0.0 (2015-02-04)
+
+
+#### Bug Fixes
+
+* **package.json:** Fix location of main ([469a7ed7](https://github.com/paradox41/angular-chartist.js/commit/469a7ed7d80d166dbd9f288ee16fd1c73e9df0ff))
+
+
+## 3.0.0 (2015-01-22)
+
+#### Bug Fixes
+
+* **angular-chartist:** Fixes chart not updating when chartType changes. ([df40605](https://github.com/paradox41/angular-chartist.js/commit/df40605210ff9330e9f363e39fbbe2d5296a570a))
+
+#### Breaking Changes
+
+* File name changed to `angular-chartist.js`
+
+## 2.0.0 (2014-12-03)
+
+
+#### Bug Fixes
+
+* **module:** Change the module name to angular-chartist ([f62d3483](https://github.com/paradox41/angular-chartist.js/commit/f62d3483e5a3d5de8715430c94f3e2389b1e521f))
+
+
+#### Breaking Changes
+
+* ngChartist is no longer a valid module name.
+
+Please update your module name to `angular-chartist`
+
+ ([f62d3483](https://github.com/paradox41/ng-chartistjs/commit/f62d3483e5a3d5de8715430c94f3e2389b1e521f))
+
---
+++ www/lib/angular-chartist.js/gulpfile.js
@@ -0,0 +1,145 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var tagVersion = require('gulp-tag-version');
+var wrap = require('gulp-wrap-umd');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var fs = require('fs');
+var changelog = require('conventional-changelog');
+
+// testing
+var karma = require('karma').server;
+
+var config = {
+    source: 'src',
+    dist: 'dist',
+    example: 'example'
+};
+
+var release = function(importance) {
+    gulp.src(['./bower.json', './package.json'])
+        .pipe($.bump({
+            type: importance
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe($.git.commit('chore: bumps package version'))
+        .pipe($.filter('bower.json'))
+        .pipe(tagVersion());
+};
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './example'
+        }
+    });
+});
+
+gulp.task('changelog', function(done) {
+    function changeParsed(err, log) {
+        if (err) {
+            return done(err);
+        }
+        fs.writeFile('CHANGELOG.md', log, done);
+    }
+    fs.readFile('./package.json', 'utf8', function(err, data) {
+        var ref$ = JSON.parse(data);
+        var repository = ref$.repository;
+        var version = ref$.version;
+
+        changelog({
+            repository: repository.url,
+            version: version
+        }, changeParsed);
+    });
+});
+
+gulp.task('clean', function(cb) {
+    require('del')([config.dist, config.example + '/lib'], {
+        force: true
+    }, cb);
+});
+
+gulp.task('enforce', function() {
+    var fs = require('fs');
+    var validateCommit = '.git/hooks/commit-msg';
+
+    if (!fs.existsSync(validateCommit)) {
+        // copy the file over
+        fs.createReadStream('./validate-commit-msg.js')
+        .pipe(fs.createWriteStream(validateCommit));
+        // make it executable
+        fs.chmodSync(validateCommit, '0755');
+    }
+});
+
+gulp.task('jshint', function() {
+    return gulp.src(config.source + '/*.js')
+        .pipe($.plumber())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('serve', ['browser-sync'], function() {
+    gulp.watch(config.source + '/*.js', [
+        'js',
+        browserSync.reload
+    ]);
+});
+
+gulp.task('js', ['jshint', 'clean'], function() {
+    return gulp.src(config.source + '/*.js')
+        .pipe(wrap({
+            exports: 'angularChartist',
+            namespace: 'angularChartist',
+            deps: [{
+                name: 'angular',
+                paramName: 'angular',
+                globalName: 'angular'
+            }, {
+                name: 'chartist',
+                paramName: 'Chartist',
+                globalName: 'Chartist'
+            }]
+        }))
+        .pipe($.beautify())
+        .pipe(gulp.dest(config.dist))
+        .pipe(gulp.dest(config.example + '/lib'))
+        .pipe($.uglify())
+        .pipe($.rename({
+            extname: '.min.js'
+        }))
+        .pipe(gulp.dest(config.dist))
+        .pipe(gulp.dest(config.example + '/lib'));
+});
+
+gulp.task('test', function(done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js'
+    }, done);
+});
+
+gulp.task('default', [
+    'js',
+    'serve'
+]);
+
+gulp.task('dist', [
+    'js'
+]);
+
+gulp.task('patch', ['dist', 'changelog'], function() {
+    return release('patch');
+});
+
+gulp.task('feature', ['dist', 'changelog'], function() {
+    return release('minor');
+});
+
+gulp.task('release', ['dist', 'changelog'], function() {
+    return release('major');
+});
