f99cab5297bbe002a80bcb4125a4712de60c060d
HerrFolgreich <me@herrfolgreich.de>
2015-05-09 06:44:15 +0200

    Added main view progress tracker

--- angular/controllers/main.js
+++ angular/controllers/main.js
@@ -11,19 +11,78 @@ angular.module('nofApp')
     $state.go('intro');
   };
   
-  $scope.count = {
-    main: {
-      value: 2,
-      unit: 'weeks'
+  var now = Math.floor(Date.now() / 1000);
+  var lastFap = Math.floor(Date.now() / 1000) - 1118640;
+  
+  $scope.hasInterval = function(i) {
+    var ary = $scope.progress.getNamedArray();
+    return !typeof(ary[i] === 'undefined');
+  };
+  
+  $scope.progress = {
+    delta: new DeltaDate(now - lastFap),
+    isDefined: function(i) {
+      var asd = !(typeof(this.delta.getNamedArray()[i]) === 'undefined');
+      return asd;
     },
-    side: {
-      value: 4,
-      unit: 'days',
-      isHidden: function() {
-        if (this.value === 0) {
-          return 'hidden';
-        }
+    getString: function(i) {
+      if (this.isDefined(i)) {
+        console.log('yolo');
+        return this.delta.getNamedArray()[i].to_s();
+      };
+    },
+    isHidden: function(i) {
+      if (isDefined(i)) {
+        return 'hidden';
       }
     }
-  }
+  };
+  console.log($scope.progress.delta.getNamedArray());
 });
+
+function DeltaDate(delta) {
+  var s_day = (60 * 60 * 24);
+  var s_week = (s_day * 7);
+  var s_year = (s_day * 365);
+  this.delta = delta;
+  
+  this.getYears = function() {
+    return Math.floor(this.delta / s_year);
+  };
+  this.getWeeks = function() {
+    return Math.floor((this.delta - (s_year * this.getYears())) / s_week);
+  };
+  this.getDays = function() {
+    return Math.floor((this.delta % s_week) / s_day);
+  };
+  
+  this.getNamedArray = function() {
+    var day = ['day', 'days'];
+    var week = ['week', 'weeks'];
+    var year = ['year', 'years'];
+    var ary = [];
+    
+    if (this.getYears() > 0) {
+      ary.push(this.pluralize(this.getYears(), year));
+    };
+    if (this.getWeeks() > 0) {
+      ary.push(this.pluralize(this.getWeeks(), week));
+    };
+    if (this.getDays() > 0) {
+      ary.push(this.pluralize(this.getDays(), day));
+    };
+    return ary;
+  };
+  
+  this.pluralize = function(number, word) {
+    if (-1 <= number && number <= 1) {
+      return {value: number, unit: word[0], to_s: function() {
+        return this.value + " " + this.unit;
+      }};
+    } else {
+      return {value: number, unit: word[1], to_s: function() {
+        return this.value + " " + this.unit;
+      }};
+    };
+  };
+};
