703cfa6103e76cbead587eddd5c26d2cfef1d5d4
HerrFolgreich <me@herrfolgreich.de>
2015-05-14 00:32:04 +0200

    Added asset minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,70 @@
+var bower = require('bower');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var rm = require('gulp-rm');
 var sh = require('shelljs');
 
+
 var paths = {
-  sass: ['./scss/**/*.scss'],
-  js:   ['./angular/**/*.js']
+  sass:   ['./scss/**/*.scss'],
+  css:    './www/css/',
+  ngJs:  ['./angular/**/*.js'],
+  js:     './www/js/'
 };
 
 gulp.task('default', ['sass', 'js']);
+gulp.task('build-assets', ['minify-css', 'minify-js']);
 
 gulp.task('sass', function(done) {
-  //gulp.src(['./scss/ionic.app.scss', './scss/app.scss'])
-  gulp.src(paths.sass)
+  gulp.src(['./scss/app.scss', './scss/ionic.app.scss'])
     .pipe(sass({
       errLogToConsole: true
     }))
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.css))
     .on('end', done);
 });
 
-gulp.task('js', function() {
-  gulp.src(paths.js)
+gulp.task('minify-css', ['sass'], function(done) {
+  gulp.src(['./www/css/*.css', '!./www/css/*.min.css'])
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    // Uncomment to keep separate .min.css files
+    //.pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(paths.css))
+    .on('end', done)
+});
+
+gulp.task('js', function(done) {
+  // delete sourcemaps to keep clean
+  gulp.src('./www/js/*.js.map', { read: false })
+    .pipe(rm());
+  gulp.src(paths.ngJs)
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('./www/js/'))
+    .pipe(gulp.dest(paths.js))
+    .on('end', done)
+});
+
+gulp.task('minify-js', ['js'], function(done) {
+  gulp.src(['./www/js/*.js', '!./www/js/*.min.js'])
+    .pipe(sourcemaps.init())
+      .pipe(uglify())
+      // Uncomment to keep separate .min.js files
+      //.pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.js))
+    .on('end', done)
 });
 
 gulp.task('watch', ['sass', 'js'], function() {
   gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.ngJs, ['js']);
 });
 
 gulp.task('install', ['git-check'], function() {
